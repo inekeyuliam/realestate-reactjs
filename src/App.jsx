ae99906e393f84e31dcf9c54feb497dfdfb322ff
=inekeyuliam <inekemargareta20@gmail.com>
2023-06-09 23:01:52 +0700

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/hero/hero";
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
     </div>
   );
 }
